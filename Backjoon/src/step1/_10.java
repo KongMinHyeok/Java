ca3ffe34e35e15095505a722b3266338e5e7a723
dlwlzjffj1 <dlwlzjffj1@naver.com>
2022-08-24 13:52:36 +0900

    백준

--- Backjoon/src/step1/_10.java
+++ Backjoon/src/step1/_10.java
@@ -1,5 +1,24 @@
 package step1;
 
+import java.util.Scanner;
+
 public class _10 {
 
+	public static void main(String[] args) {
+	
+		int arr [] = {};
+		
+		Scanner sc = new Scanner(System.in);
+		int q = sc.nextInt();
+		int w = sc.nextInt();
+		int e = sc.nextInt();
+		int r = sc.nextInt();
+		int t = sc.nextInt();
+		int y = sc.nextInt();
+		
+		System.out.println(0 );
+		
+	}
+	
+	
 }
