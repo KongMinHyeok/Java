97ae958bf09d691ec948d2e5f1db3769b915cd4c
unknown <dlwlzjffj1@naver.com>
2022-09-05 13:24:23 +0900

    2022/09/05 예외처리 실습

---
+++ Ch07/src/sub1/Animal.java
@@ -0,0 +1,15 @@
+package sub1;
+
+public class Animal {
+	
+	
+	public void move() {
+		System.out.println("move....");
+	}
+	
+	
+	public void hunt() {
+		System.out.println("hunt....");
+		
+	}
+}
